361a314d46f943b9fe5d4956b625c580119a9574
Kevin <kevin.gosselin29@gmail.com>
2025-11-26 11:23:59 +0100

    mise a jour App/Model/console.php

--- App/Model/Console.php
+++ App/Model/Console.php
@@ -5,8 +5,37 @@ namespace App\Model;
 class Console
 {
     //Attributs
+    private int $id;
+    private string $name;
+    private string $manufacturer;
 
     //Constructeur
+    public function __construct() {}
 
     //Getters et Setters
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function getManufacturer(): string
+    {
+        return $this->manufacturer;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+    public function setManufacturer(string $manufacturer): void
+    {
+        $this->manufacturer = $manufacturer;
+    }
 }
