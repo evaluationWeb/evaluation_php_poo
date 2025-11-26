075b7e6d0f69539ab5717a6f321d4a982db2564e
Kevin <kevin.gosselin29@gmail.com>
2025-11-26 10:53:00 +0100

    creation base de donnée

--- bdd.sql
+++ bdd.sql
@@ -1,1 +1,24 @@
 -- Ecrire votre code SQL dans ce fichier
+CREATE DATABASE IF NOT EXISTS games;
+use games;
+
+CREATE TABLE console(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    manufacturer VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE video_game (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    type VARCHAR(100) NOT NULL,
+    publish_at DATE NOT NULL,
+    console_id INT NOT NULL,
+    FOREIGN Key (console_id) REFERENCES console(id)
+);
+
+INSERT INTO console (name,manufacturer) VALUES
+('PlayStation 5', 'Sony'),
+('Switch', 'Nintendo'),
+('Xbox Series X', 'Microsoft'),
+('Steam Deck', 'Valve');
