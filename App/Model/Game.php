171038b2d712d59676284f9b7f27cb01d267ddab
Kevin <kevin.gosselin29@gmail.com>
2025-11-26 11:51:34 +0100

    mise a jour App/Model/game.php

--- App/Model/Game.php
+++ App/Model/Game.php
@@ -5,8 +5,55 @@ namespace App\Model;
 class Game
 {
     //Attributs
+    private int $id;
+    private string $title;
+    private string $type;
+    private string $publish_at;
+    private int $console_id;
 
     //Constructeur
+    public function __construct() {}
 
     //Getters et Setters
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+    public function getType(): string
+    {
+        return $this->type;
+    }
+    public function getPublishAt(): string
+    {
+        return $this->publish_at;
+    }
+    public function getConsoleId(): int
+    {
+        return $this->console_id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+    public function setPublishAt(string $publish_at): void
+    {
+        $this->publish_at = $publish_at;
+    }
+    public function setConsoleId(int $console_id): void
+    {
+        $this->console_id = $console_id;
+    }
 }
